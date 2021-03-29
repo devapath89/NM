6f0d375738f207020f87e910d8dadbeade316218
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-29 10:45:44 +0200

    file2
    added file2

--- oblig/Equipment.js
+++ oblig/Equipment.js
@@ -1,54 +1,65 @@
-
+//Equipments.Ability[x].file
 const Equipments = {
-"Ability":{ 
-    "light-fang":{
-        Attack : 5,
-        file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Lightfang.jpg?raw=true'},
-    "shadow-thunder-blast":{
-        Nin: 10,
-        Attack : 5,
-        file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Shadowthunder.png?raw=true'},
-    "limbo":{
-        Tai:10,
-        Nin: 10,
-         Attack : 5,
-        file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Limbo.jpg?raw=true'},
-    "divine-genesis":{
-        Nin: 30,
-        Gen: 30,
-        file:'https://github.com/devapath89/NM/blob/main/oblig/Img/divinegenesis.jpg?raw=true'},
-        "hair-camouflage":{
-             Nin: 11,
-            file:''},
-        "flower-petal-escape":{
-            Nin: 15,
-            Gen: 20,
-            file:''},
-        "evil-sealing":{
-            Nin:15,
-            Gen:20,
-            GenjutsuActivation:10,
-            file:''},   
-                
-        "string-bean-binding":{
-            Nin:10,
-            Gen:20,
-            GenjutsuMastery:20,
-            file:''},
+    "Ability":{ 
+        "light-fang":{
+            Attack : 5,
+            file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Lightfang.jpg?raw=true'
+        },
+        "shadow-thunder-blast":{
+            Nin: 10,
+            Attack : 5,
+            file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Shadowthunder.png?raw=true'
+        },
+        "limbo":{
+            Tai:10,
+            Nin: 10,
+             Attack : 5,
+            file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Limbo.jpg?raw=true'
+        },
+        "divine-genesis":{
+            Nin: 30,
+            Gen: 30,
+            file:'https://github.com/devapath89/NM/blob/main/oblig/Img/divinegenesis.jpg?raw=true'
+        },
+            "hair-camouflage":{
+                 Nin: 11,
+                file:''
+            },
+            "flower-petal-escape":{
+                Nin: 15,
+                Gen: 20,
+                file:''
+            },
+            "evil-sealing":{
+                Nin:15,
+                Gen:20,
+                GenjutsuActivation:10,
+                file:''
+            },   
+                    
+            "string-bean-binding":{
+                Nin:10,
+                Gen:20,
+                GenjutsuMastery:20,
+                file:''
+            },
             "paper-shuriken":{
-                Nin:4,
-                Buki:8,
-                file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Papershurikent.png?raw=true'},
+                 Nin:4,
+                 Buki:8,
+                file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Papershurikent.png?raw=true'
+            },
             "paper-chakram":{
                 Nin:6,
                 Buki:10,
                 BukijutsuBoost:4,
-                file:'https://github.com/devapath89/NM/blob/main/oblig/Img/paperChakram.png?raw=true'},
+                file:'https://github.com/devapath89/NM/blob/main/oblig/Img/paperChakram.png?raw=true'
+            },
             "paper-clone":{
                 Nin:16,
                 Buki:15,
                 Sta:8,
-                file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Paper%20clone.png?raw=true'},
+                file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Paper%20clone.png?raw=true'
+            },
             "sacred-paper-emissary":{
                 Nin:10,
                 Buki:10,
@@ -57,11 +68,5 @@ const Equipments = {
                 Reroll:10,
                 file:''
             }
-        
-       
-           
-         
-        
         }              
-}
-
+    }
