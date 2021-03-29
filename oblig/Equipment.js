93b4436a25a66544e18315c27479a45c5a920372
devapath89 <72752461+devapath89@users.noreply.github.com>
2021-03-29 12:07:47 +0200

    s

--- oblig/Equipment.js
+++ oblig/Equipment.js
@@ -3,37 +3,44 @@ const Equipments = {
     "Ability":{ 
         // "light-fang":{
         //     Attack : 5,
+        //      file:'',
         //     file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Lightfang.jpg?raw=true'
         // },
         // "shadowthunderblast":{
         //     Nin: 10,
         //     Attack : 5,
+        //      file:'',
         //     // file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Shadowthunder.jpg?raw=true'
         // },
         // "limbo":{
         //     Tai:10,
         //     Nin: 10,
         //      Attack : 5,
-        //     file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Limbo.jpg?raw=true'
+        //      file:'',
+        //     file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Limbo.jpg?raw=true'
         // },
         // "divine-genesis":{
         //     Nin: 30,
         //     Gen: 30,
-        //     file:'https://github.com/devapath89/NM/blob/main/oblig/Img/divinegenesis.jpg?raw=true'
+        //      file:'',
+        //     file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/divinegenesis.jpg?raw=true'
         //  },
         //     "hair-camouflage":{
         //          Nin: 11,
+        //      file:'',
         //         file:''
         //     },
         //     "flower-petal-escape":{
         //         Nin: 15,
         //         Gen: 20,
+        //      file:'',
         //         file:''
         //     },
         //     "evil-sealing":{
         //         Nin:15,
         //         Gen:20,
         //         GenjutsuActivation:10,
+        //      file:'',
         //         file:''
         //     },   
                     
@@ -41,24 +48,28 @@ const Equipments = {
         //         Nin:10,
         //         Gen:20,
         //         GenjutsuMastery:20,
+        //      file:'',
         //         file:''
         //     },
             // "paper-shuriken":{
             //      Nin:4,
             //      Buki:8,
-            //     file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Papershurikent.png?raw=true'
+            //      file:'',
+            //     file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Papershurikent.png?raw=true'
             // },
             //  "paper-chakram":{
             //    Nin:6,
             //     Buki:10,
             //      BukijutsuBoost:4,
-            //      file:'https://github.com/devapath89/NM/blob/main/oblig/Img/paperChakram.jpg?raw=true'
+            //      file:'',
+            //      file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/paperChakram.jpg?raw=true'
             // },
             // "paper-clone":{
             //     Nin:16,
             //     Buki:15,
             //     Sta:8,
-            //     file:'https://github.com/devapath89/NM/blob/main/oblig/Img/Paper%20clone.jpg?raw=true'
+            //      file:'',
+            //     file2:'https://github.com/devapath89/NM/blob/main/oblig/Img/Paper%20clone.jpg?raw=true'
             // },
         //     "sacred-paper-emissary":{
         //         Nin:10,
@@ -66,6 +77,7 @@ const Equipments = {
         //         Sta:10,
         //         Ele:5,
         //         Reroll:10,
+        //      file:'',
         //         file:''
         //     }
         
